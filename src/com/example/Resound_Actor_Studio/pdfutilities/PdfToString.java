517e1b2c9df77872bfad014ab7e43375e82183dc
JerellDR <jdub03@gmail.com>
2013-12-18 14:18:30 -0500

    delete unecessary files

--- src/com/example/Resound_Actor_Studio/pdfutilities/PdfToString.java
+++ src/com/example/Resound_Actor_Studio/pdfutilities/PdfToString.java
@@ -1,11 +1,8 @@
 package com.example.Resound_Actor_Studio.pdfutilities;
 
 /**
- * Created with IntelliJ IDEA.
  * User: mrgadgetz
  * Date: 12/17/13
- * Time: 5:29 PM
- * To change this template use File | Settings | File Templates.
  */
 public class PdfToString {
 
